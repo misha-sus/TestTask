665974862e16be6f545e3a0c82db17308ed698e1
Misha Khush <Misha26099777@yandex.ru>
2022-03-25 13:34:38 +0300

    Junit5 close

--- src/test/java/AllSelenide/SelenideDriver.java
+++ src/test/java/AllSelenide/SelenideDriver.java
@@ -13,38 +13,32 @@ public class SelenideDriver {
 
     //открывает сайт
     public static void open(String URL) {
-        try {
-            ChromeOptions options = new ChromeOptions();
-            options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-            System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
-            driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-            driver.manage().deleteAllCookies();
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            driver.get(URL);
-        } catch (Exception e) {
-            close();
-        }
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL);
+
     }
+
     // локтор xpath
     public static WebElement $x(String xpath) {
-        try {
-            element = driver.findElement(By.xpath(xpath));
-        } catch (Exception e) {
-            close();
-        }
+        element = driver.findElement(By.xpath(xpath));
+
         return element;
     }
+
     //локатор CSS
     public static WebElement $(String cssSelector) {
-        try {
-            element = driver.findElement(By.cssSelector(cssSelector));
-        } catch (Exception e) {
-            close();
-        }
+
+        element = driver.findElement(By.cssSelector(cssSelector));
         return element;
     }
-     //закрывает браузер
+
+    //закрывает браузер
     public static void close() {
         driver.quit();
     }
